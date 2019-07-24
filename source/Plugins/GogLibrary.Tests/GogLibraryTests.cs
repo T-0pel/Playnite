2c5dd969bb318f7c6883316c634b72073e757bad
Tomas Oplatek <oplatek.t@gmail.com>
2019-07-24 19:35:20 +0100

    Add GOG plugin unit tests

--- source/Plugins/GogLibrary.Tests/GogLibraryTests.cs
+++ source/Plugins/GogLibrary.Tests/GogLibraryTests.cs
@@ -1,31 +1,57 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using Playnite.SDK.Models;
+using Playnite.Tests;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-using Playnite.SDK.Models;
-using Playnite;
-using Moq;
-using Playnite.SDK;
-using Playnite.Tests;
 
 namespace GogLibrary.Tests
 {
     [TestFixture]
     public class GogLibraryTests
     {
-        public static GogLibrary CreateLibrary()
+        public Mock<GogLibrary> GetGogLibraryMock()
         {
-            return new GogLibrary(PlayniteTests.GetTestingApi().Object);
+            var gogLib = new Mock<GogLibrary>(MockBehavior.Loose, PlayniteTests.GetTestingApi().Object);
+
+            const string installedGameId = "installedGameId";
+            var installedGame = new GameInfo
+            {
+                InstallDirectory = @"C:\Temp",
+                GameId = installedGameId,
+                Source = "GOG",
+                Name = "installedGameName",
+                IsInstalled = true
+            };
+            var installedGames = new Dictionary<string, GameInfo> { { installedGameId, installedGame } };
+
+            const string uninstalledGameId = "uninstalledGameId";
+            var uninstalledGame = new GameInfo
+            {
+                InstallDirectory = @"C:\Temp",
+                GameId = uninstalledGameId,
+                Source = "GOG",
+                Name = "uninstalledGameName",
+                Playtime = 1,
+                LastActivity = DateTime.MinValue
+            };
+            var libraryGames = new List<GameInfo>
+            {
+                uninstalledGame,
+                new GameInfo { GameId = installedGame.GameId, Playtime = 2, LastActivity = DateTime.MaxValue }
+            };
+
+            gogLib.Setup(a => a.GetInstalledGames()).Returns(() => installedGames);
+            gogLib.Setup(a => a.GetLibraryGames()).Returns(() => libraryGames);
+
+            return gogLib;
         }
 
         [Test]
         public void GetInstalledGamesRegistry()
         {
-            var gogLib = CreateLibrary();
-            var games = gogLib.GetInstalledGames();
+            var games = GetGogLibraryMock().Object.GetInstalledGames();
             Assert.AreNotEqual(0, games.Count);
             CollectionAssert.AllItemsAreUnique(games);
 
@@ -39,5 +65,10 @@ namespace GogLibrary.Tests
             }
         }
 
+        [Test]
+        public void ImportOnlyInstalled_WithoutStatsTest()
+        {
+
+        }
     }
 }
